014904a80a89bb4c7d303e878162aedbf77b6562
maks1596 <386125Mm>
2021-02-28 21:25:18 +0300

    Добавлена сущность "Отряд"

---
+++ model/src/main/java/com/example/model/Squad.kt
@@ -0,0 +1,17 @@
+package com.example.model
+
+/**
+ * Отряд
+ */
+data class Squad(
+
+    /**
+     * Название отряда
+     */
+    val name: String,
+
+    /**
+     * Сила отряда, численность
+     */
+    val strength: Int
+)
