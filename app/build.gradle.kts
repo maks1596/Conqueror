8b1690945b7d1dc509fbc4b5d6889fccdba9fc70
maks1596 <386125Mm>
2021-02-28 20:12:40 +0300

    Убрана зависимость на стандартную библиотеку kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,9 +28,7 @@ android {
 }
 
 dependencies {
-    val kotlinVersion: String by extra
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.3.0")
 }
