8938b93e109700b60f467a92257ad635304a192f
maks1596 <386125Mm>
2021-02-28 21:53:10 +0300

    Добавлена сущность "Территория"

---
+++ model/src/main/java/com/example/model/Land.kt
@@ -0,0 +1,26 @@
+package com.example.model
+
+/**
+ * Территория для захвата
+ */
+data class Land(
+    /**
+     * Название территории
+     */
+    val name: String,
+
+    /**
+     * Вознаграждение за захват территории
+     */
+    val reward: Int,
+
+    /**
+     * Вероятность успешного захвата территории
+     */
+    val successProbability: Double,
+
+    /**
+     * Расстояние до территории
+     */
+    val distance: Double
+)
