9dda839ed2859f2a5b46c54330d83696df5f238c
maks1596 <386125Mm>
2021-03-07 17:03:15 +0300

    Упрощён FragmentFactoryModule

--- app/src/main/java/com/example/conqueror/di/fragmentFactory/FragmentFactoryModule.kt
+++ app/src/main/java/com/example/conqueror/di/fragmentFactory/FragmentFactoryModule.kt
@@ -2,12 +2,9 @@ package com.example.conqueror.di.fragmentFactory
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
-import com.example.conqueror.MainFragment
 import com.example.conqueror.di.MainModule
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
-import dagger.multibindings.IntoMap
 import javax.inject.Provider
 
 @Module(
@@ -15,14 +12,10 @@ import javax.inject.Provider
         MainModule::class
     ]
 )
-internal abstract class FragmentFactoryModule {
+internal class FragmentFactoryModule {
 
-    companion object {
-
-        @Provides
-        fun provideFragmentFactory(
-            providers: Map<Class<out Fragment>, @JvmSuppressWildcards Provider<Fragment>>
-        ): FragmentFactory = InjectFragmentFactory(providers)
-    }
-
+    @Provides
+    fun provideFragmentFactory(
+        providers: Map<Class<out Fragment>, @JvmSuppressWildcards Provider<Fragment>>
+    ): FragmentFactory = InjectFragmentFactory(providers)
 }
