eecfa8c21fdeebd1dd2f7761f13540cae5a7986c
maks1596 <386125Mm>
2021-10-03 15:14:51 +0300

    Прописана генерация отрядов

--- data-impl/src/main/java/com/netology/data/impl/SquadRepositoryImpl.kt
+++ data-impl/src/main/java/com/netology/data/impl/SquadRepositoryImpl.kt
@@ -4,7 +4,30 @@ import com.example.model.Squad
 import com.netology.data.AmountFilter
 import com.netology.data.SquadRepository
 
-class SquadRepositoryImpl : SquadRepository {
+class SquadRepositoryImpl(
+    firstSquadStrength: Int
+) : SquadRepository {
+
+    private val squads = generateSequence(
+        seed = Squad(
+            race = Squad.Race.Human,
+            specialization = Squad.Specialization.Melee,
+            strength = firstSquadStrength
+        )
+    ) { prevSquad ->
+        val nextRace = Squad.Race.values().getOrNull(prevSquad.race.ordinal + 1)
+        if (nextRace == null) null else {
+            val nextSpecialization = Squad.Specialization.values().getOrElse(
+                prevSquad.specialization.ordinal + 1
+            ) { Squad.Specialization.Melee }
+
+            Squad(
+                race = nextRace,
+                specialization = nextSpecialization,
+                strength = prevSquad.strength shl 1
+            )
+        }
+    }
 
     override fun getSquads(
         amountFilter: AmountFilter?,
@@ -12,5 +35,4 @@ class SquadRepositoryImpl : SquadRepository {
     ): List<Squad> {
         TODO("Not yet implemented")
     }
-
 }
