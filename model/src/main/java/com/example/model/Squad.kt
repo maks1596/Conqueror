6335daed1a18bd282108b84517ed535d7586d5da
maks1596 <386125Mm>
2021-10-03 13:43:31 +0300

    Название отряда заменены на расу и специализацию

--- model/src/main/java/com/example/model/Squad.kt
+++ model/src/main/java/com/example/model/Squad.kt
@@ -6,12 +6,65 @@ package com.example.model
 data class Squad(
 
     /**
-     * Название отряда
+     * Раса отряда
      */
-    val name: String,
+    val race: Race,
+
+    /**
+     * Специализация отряда
+     */
+    val specialization: Specialization,
 
     /**
      * Сила отряда, численность
      */
     val strength: Int
-)
+) {
+
+    /**
+     * Раса отряда
+     */
+    enum class Race {
+
+        /**
+         * Люди
+         */
+        Human,
+
+        /**
+         * Эльфы
+         */
+        Elf,
+
+        /**
+         * Орки
+         */
+        Orc,
+
+        /**
+         * Гномы
+         */
+        Dwarf
+    }
+
+    /**
+     * Специализация отряда
+     */
+    enum class Specialization {
+
+        /**
+         * Ближний бой
+         */
+        Melee,
+
+        /**
+         * Дистанционная атака
+         */
+        Range,
+
+        /**
+         * Магия
+         */
+        Magic
+    }
+}
